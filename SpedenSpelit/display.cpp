b274404e0f09eb17ceb6d6e76156bde61f179e61
mhaapea <mihaapea@gmail.com>
2025-09-25 18:10:20 +0300

    Update display.cpp

--- SpedenSpelit/display.cpp
+++ SpedenSpelit/display.cpp
@@ -1,10 +1,10 @@
 #include "display.h"
 
-int resetPin = 12;    //kytketty SRCLR(shift register clear), käytetään resetti pinninä
-int clockPin = 11;    //kytketty SRCLK(shift register clock), data siirtyy siirtorekisteriin kun signaali vaihtuu LOW -> HIGH
-int latchPin = 10;    //kytketty RCLK(register clock/latch pin), data siirtyy siirtorekisteristä, kun signaali vaihtuu LOW -> HIGH
-int enablePin = 9;    //kytketty OE(output enable), toimii kytkimenä outputeille. LOW asemassa rekisterin output pinnit auki, HIGH asemassa kiinni.
-int dataPin = 8;      //kytketty SER(serial input), siirtää datan bitti kerrallaan siirtorekisterille.
+char resetPin = 12;    //kytketty SRCLR(shift register clear), käytetään resetti pinninä
+char clockPin = 11;    //kytketty SRCLK(shift register clock), data siirtyy siirtorekisteriin kun signaali vaihtuu LOW -> HIGH
+char latchPin = 10;    //kytketty RCLK(register clock/latch pin), data siirtyy siirtorekisteristä, kun signaali vaihtuu LOW -> HIGH
+char enablePin = 9;    //kytketty OE(output enable), toimii kytkimenä outputeille. LOW asemassa rekisterin output pinnit auki, HIGH asemassa kiinni.
+char dataPin = 8;      //kytketty SER(serial input), siirtää datan bitti kerrallaan siirtorekisterille.
 
 byte segLuku[] = {
   // tähän taulukkoon mitä datapinni lähettää sarjarekisterille kun halutaan näyttää
@@ -17,6 +17,8 @@ byte segLuku[] = {
 
 void initializeDisplay(void)
 {
+  cli();
+
   pinMode(resetPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);        //lähtöpinnit OUTPUT moodiin
@@ -24,13 +26,15 @@ void initializeDisplay(void)
   pinmode(dataPin, OUTPUT);
   digitalWrite(resetPin, LOW);      //LOW resettaa sarjarekisterin kaikki bitit nolliksi
   digitalWrite(resetPin, HIGH);     //poistaa resetin
+
+  sei();
 }
 
 
 void writeByte(uint8_t bits,bool last)
 {
   pinMode(latchPin, LOW);   //valmistellaan datan siirto
-                            //tähän jotain
+  //tähän jotain
   pinMode(latchPin, HIGH);  //data siirtyy siirtorekisteristä  
 }
 
