1ece55be08b2337da99e54e9ef9c1e8447cef71f
mhaapea <mihaapea@gmail.com>
2025-09-26 19:03:01 +0300

    Bittiarvot lisätty numeroille 0-9, writeByte funktio lisätty

--- SpedenSpelit/display.cpp
+++ SpedenSpelit/display.cpp
@@ -1,18 +1,22 @@
 #include "display.h"
 
-char resetPin = 12;    //kytketty SRCLR(shift register clear), käytetään resetti pinninä
-char clockPin = 11;    //kytketty SRCLK(shift register clock), data siirtyy siirtorekisteriin kun signaali vaihtuu LOW -> HIGH
-char latchPin = 10;    //kytketty RCLK(register clock/latch pin), data siirtyy siirtorekisteristä, kun signaali vaihtuu LOW -> HIGH
-char enablePin = 9;    //kytketty OE(output enable), toimii kytkimenä outputeille. LOW asemassa rekisterin output pinnit auki, HIGH asemassa kiinni.
-char dataPin = 8;      //kytketty SER(serial input), siirtää datan bitti kerrallaan siirtorekisterille.
+char resetPin = 12;    // kytketty SRCLR(shift register clear), käytetään resetti pinninä
+char clockPin = 11;    // kytketty SRCLK(shift register clock), data siirtyy siirtorekisteriin kun signaali vaihtuu LOW -> HIGH
+char latchPin = 10;    // kytketty RCLK(register clock/latch pin), data siirtyy siirtorekisteristä, kun signaali vaihtuu LOW -> HIGH
+char enablePin = 9;    // kytketty OE(output enable), toimii kytkimenä outputeille. LOW asemassa rekisterin output pinnit auki, HIGH asemassa kiinni.
+char dataPin = 8;      // kytketty SER(serial input), siirtää datan bitti kerrallaan siirtorekisterille.
 
 byte segLuku[] = {
-  // tähän taulukkoon mitä datapinni lähettää sarjarekisterille kun halutaan näyttää
-  // jokin tietty luku
-  // esim.
-  // B00101010, // 1
-  // B01000010, // 2
-  // B10010011, // 3 jne., nämä ei ole vielä oikein
+  B00111111, 	// 0
+  B00001001, 	// 1
+  B01011110, 	// 2
+  B01011011,	// 3
+  B01101001,	// 4
+  B01110011,	// 5
+  B01110111,	// 6
+  B00011001,	// 7
+  B01111111,	// 8
+  B01111001 	// 9
 }
 
 void initializeDisplay(void)
@@ -21,11 +25,12 @@ void initializeDisplay(void)
 
   pinMode(resetPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  pinMode(latchPin, OUTPUT);        //lähtöpinnit OUTPUT moodiin
+  pinMode(latchPin, OUTPUT);        // lähtöpinnit OUTPUT moodiin
   pinMode(enablePin, OUTPUT);
   pinmode(dataPin, OUTPUT);
-  digitalWrite(resetPin, LOW);      //LOW resettaa sarjarekisterin kaikki bitit nolliksi
-  digitalWrite(resetPin, HIGH);     //poistaa resetin
+  digitalWrite(resetPin, LOW);      // LOW resettaa sarjarekisterin kaikki bitit nolliksi
+  digitalWrite(resetPin, HIGH);     // poistaa resetin
+  digitalWrite(enablePin, LOW);     // pidetään output pinnit auki
 
   sei();
 }
@@ -33,9 +38,11 @@ void initializeDisplay(void)
 
 void writeByte(uint8_t bits,bool last)
 {
-  pinMode(latchPin, LOW);   //valmistellaan datan siirto
-  //tähän jotain
-  pinMode(latchPin, HIGH);  //data siirtyy siirtorekisteristä  
+  // bool last on sitä varten jos rekisterit on kytketty toisiinsa
+  // jos käytetään pin 13 toisen rekisterin dataPinninä niin sitä ei varmaan tarvi
+  digitalWrite(latchPin, LOW);   //valmistellaan datan siirto
+  shiftOut(dataPin, clockPin, LSBFIRST, bits);	//Siirretään bitit rekisterille
+  digitalWrite(latchPin, HIGH);  //data siirtyy siirtorekisteriltä ulostuloihin
 }
 
 
