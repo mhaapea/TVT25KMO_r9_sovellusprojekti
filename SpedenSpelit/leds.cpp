bb2c714c8e9a8dce389654d043cbdda196b57162
Onni Flink <onni.flink@gmail.com>
2025-10-01 16:14:40 +0300

    ledit

--- SpedenSpelit/leds.cpp
+++ SpedenSpelit/leds.cpp
@@ -1,36 +1,63 @@
 #include "leds.h"
 
+const byte ledPins[4] = {A2, A3, A4, A5};
 
-void initializeLeds()
-{
-// see requirements for this function from leds.h
+void initializeLeds() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
 }
 
-void setLed(byte ledNumber)
-{
-// see requirements for this function from leds.h
-
+void setLed(byte ledNumber) {
+  clearAllLeds();
+  if (ledNumber < 4) {
+    digitalWrite(ledPins[ledNumber], HIGH);
+  }
 }
 
-
-void clearAllLeds()
-{
-// see requirements for this function from leds.h
- 
+void clearAllLeds() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
 }
 
-void setAllLeds()
-{
-// see requirements for this function from leds.h
+void setAllLeds() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledPins[i], HIGH);
+  }
 }
 
-
-void show1()
-{
-// see requirements for this function from leds.h
+void show1() {
+  for (int i = 0; i < 16; i++) {
+    for (int bit = 0; bit < 4; bit++) {
+      digitalWrite(ledPins[bit], (i >> bit) & 1);
+    }
+    delay(500);
+  }
+  clearAllLeds();
 }
 
-void show2(int rounds)
-{
-// see requirements for this function from leds.h  
+void show2(int rounds) {
+  int delayTime = 500;
+
+  for (int i = 0; i < rounds; i++) {
+    for (int led = 0; led < 4; led++) {
+      clearAllLeds();
+      digitalWrite(ledPins[led], HIGH);
+      delay(delayTime);
+    }
+    if (delayTime > 100) {
+      delayTime -= 20;
+    }
+  }
+  clearAllLeds();
+}
+
+void loop() {
+  byte led = random(0, 4);
+  setLed(led);
+  delay(300);
+  clearAllLeds();
+  delay(200);
 }
